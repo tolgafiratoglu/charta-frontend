1edde066aee6aecaefc957644a834db735876e2d
tftr <tolgafiratoglu@gmail.com>
2021-02-03 23:15:08 +0300

    List Item

---
+++ src/component/partials/ListItem.js
@@ -0,0 +1,42 @@
+import {Button, Col, Container, Row} from "reactstrap";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faClone, faThumbsUp, faTrashAlt} from "@fortawesome/free-regular-svg-icons";
+import {faEdit, faSpinner, faImage, faKey} from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+
+import 'react-toastify/dist/ReactToastify.css';
+import {text} from "@fortawesome/fontawesome-svg-core";
+
+import { useTranslation, withTranslation, Trans } from 'react-i18next';
+
+const ListItem = (props) => {
+
+    const { t, i18n } = useTranslation();
+
+    const [hidden, setHidden] = React.useState(false);
+
+    const id = props.id
+    const title = props.title
+    const subTitle = props.subTitle
+    const subContent = props.subContent
+
+    const updateButtonVisibility = props.updateButtonVisibility
+    const deleteButtonVisibility = props.deleteButtonVisibility
+
+    return(!hidden ? <Container key={id} className="list-container">
+               <Row>
+                    <Col className="col-md-6">
+                        <div>
+                            <span className="font-weight-bold">{title} {subContent ? '(' + subContent + ')' : ''}</span>
+                        </div>
+                    </Col>
+                    <Col className="list-buttons col-md-6">
+                        {deleteButtonVisibility == "1" ? <Button onClick={()=>{props.delete(id)}} color="warning"><FontAwesomeIcon icon={faTrashAlt}></FontAwesomeIcon> { t('commons.delete') }</Button> : ''}
+                        {updateButtonVisibility == "1" ? <Button onClick={()=>{props.edit(id)}} color="info"><FontAwesomeIcon icon={faEdit}></FontAwesomeIcon> { t('commons.edit') }</Button> : ''}
+                    </Col>
+               </Row>
+          </Container> : '')
+
+}
+
+export default ListItem;
